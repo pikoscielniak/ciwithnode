22561f75ae7e7f0a236f6f5492e30c930b260d62
Piotr <piotrekbass@interia.pl>
2015-03-05 20:08:30 +0000

    tests setup

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var mongoose = require('mongoose');
 var app = express();
 
 var jobModel = require("./models/Job");
-var mongoDb = require("./services/mongoDb");
 
 app.use(express.static('public'));
 app.set('views', __dirname);
@@ -11,7 +10,7 @@ app.set('view engine', 'jade');
 
 app.get('/api/jobs', function(req, res) {
     mongoose.model('Job').find({}).exec(function(err, collection) {
-        if(err){
+        if (err) {
             console.log(err);
         }
         res.send(collection);
@@ -22,6 +21,17 @@ app.get('*', function(req, res) {
     res.render('index');
 });
 
-mongoDb.connect();
+var dbCon = process.env.DB_CONNECTION || 'mongodb://localhost/jobfinder';
+
+mongoose.connect(dbCon);
+
+var con = mongoose.connection;
+con.once('open', function(err) {
+    if (err) {
+        return console.log('error');
+    }
+    jobModel.seedJobs();
+    console.log('connected to mongodb successfully!');
+});
 
 app.listen(process.env.PORT, process.env.IP);
--- test/jobsData.spec.js
+++ test/jobsData.spec.js
@@ -1,27 +1,33 @@
 var expect = require("chai").expect;
+var jobModel = require("../models/Job");
 var mongoose = require('mongoose');
+var dbCon = 'mongodb://localhost/jobfinder';
 
-var mongoDb = require("../services/mongoDb");
-
+function resetJobs(callback) {
+    if (mongoose.connection.collection.jobs) {
+        mongoose.connection.collection.jobs.drop(callback);
+    }
+    else {
+        callback();
+    }
+}
 
 describe('get jobs', function() {
 
-    before(function(done) {
-        if (mongoose.connection.readyState === 0) {
-            mongoDb.connect(done);
-        }
-        else {
-            done();
-        }
-    });
-
     it('should never be empty since jobs are seeded', function(done) {
-        mongoose.model('Job').find({}).exec(function(err, collection) {
-            if (err) {
-                return done(err);
-            }
-            expect(collection.length).to.be.at.least(1);
-            done();
+        mongoose.connect(dbCon, function() {
+            resetJobs(function() {
+                jobModel.seedJobs(function(err) {
+                    if(err) done(err);
+                    mongoose.model('Job').find({}).exec(function(err, collection) {
+                        if (err) {
+                            return done(err);
+                        }
+                        expect(collection.length).to.be.at.least(1);
+                        done();
+                    });
+                })
+            });
         });
     });
 });
